010cebb6df337141d9ff64d733a6f8a5665507a8
Vadym <vadim.bosakk@gmail.com>
2024-01-24 13:11:48 +0200

    first commit ever

--- homework3_Bósak.py
+++ homework3_Bósak.py
@@ -5,15 +5,16 @@ while len(products_list) != 0:
 
     if not products_upd or products_upd.isspace():
         print("Products names can't be the whitespace or empty")
-    else:
-        if products_upd.isdigit():
-            print("The products can't be numbers")
-        elif products_upd.startswith('-') and products_upd[1:] in products_list:
+
+    elif products_upd.startswith('-'):
+        if products_upd[1:] in products_list:
             products_list.remove(products_upd[1:])
             print('Product removed. Updated list: ', products_list)
-
         else:
-            products_list.append(products_upd)
-            print('Product added. Updated list: ', products_list)
+            print(f"Product '{products_upd}' not found in the list")
+
+    else:
+        products_list.append(products_upd)
+        print('Product added. Updated list: ', products_list)
 
 print("List is empty. Program finished.")
