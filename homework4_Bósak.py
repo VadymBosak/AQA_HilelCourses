e814e30a3c36382c8934cfe16b418e898e20bffb
Vadym <vadim.bosakk@gmail.com>
2024-01-27 15:14:36 +0200

    first commit ever

---
+++ homework4_Bósak.py
@@ -0,0 +1,67 @@
+# 1 - Є 3 группи людей(sets) australia_blacklist, poker_blacklist, alcohol_blacklist. \
+# В кожній групі вказані імена. Вивести тих хто виграв джекпот(є одразу в 3х списках)
+
+australia_blacklist = {'John', 'Max', 'Alex'}
+poker_blacklist = {'Hanna', 'Alex', 'Andrew'}
+alcohol_blacklist = {'Joe', 'Alex', 'Den'}
+
+jackpot_winner = australia_blacklist.intersection(poker_blacklist, alcohol_blacklist)
+print(jackpot_winner)
+print('\n')
+
+# 2 - Словник має наступні дані: {'Alex': 'house', 'Max': 'Flat', 'Olha': 'Appartments', 'Oleh': 'Trench'}
+# Використвоючі f-string вивести: "User_name is living in place_name" для кожного юзера. Використовувати цикл
+
+user_names = {'Alex': 'house', 'Max': 'Flat', 'Olha': 'Appartments', 'Oleh': 'Trench'}
+
+for name in user_names:
+    print(f'{name} is living in {user_names[name]}')
+print('\n')
+
+
+# 3 - Є список ['Jack', 'Leon', 'Alice', None, 32, 'Bob']
+# Вивести ТІЛЬКИ коректні імена(тобто стрінги). Використовувати Continue.
+
+names_list = ['Jack', 'Leon', 'Alice', None, 32, 'Bob']
+
+for name in names_list:
+    if type(name) == str:
+        print(name)
+    continue
+print('\n')
+
+
+# 4 - Порахувати та вивести(print) кількість букв в строці.
+#
+# Юзер щось вводить(input)
+# Ваша задача надрукувати кількість кожного символу того що він ввів.
+#
+# Прикдад:
+# Юзер вводить:
+# My name is Emmy Santiago.
+#
+# ВИ прінтаете щось накшталт:
+# M = 1, y = 2, n = 2, ...(або в іншому форматі, це не принциповоб головне, що б чітко було зрозуміло скільки разів
+# зустрічаеться кожна буква)
+#
+# Тобто кожну букву та скільки разів вона зустрічаеться
+#
+# Підказка: це про словники, get можна використати для того щоб витягнути чи є ключ без помилки
+# та надати дефолтне значення
+
+dict_input = input("Let's count each character amount: ")
+dict_counter = {}
+
+for character in dict_input:
+    if character.isspace():
+        continue
+    dict_counter[character] = dict_counter.get(character, 0) + 1
+
+for character, count in dict_counter.items():
+    print(f"{character}: {count}")
+
+
+
+
+
+
